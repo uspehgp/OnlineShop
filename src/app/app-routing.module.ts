6ae8d3fe0e2ff1395e518677d85f001a1bc520b1
Uspehgp <uspehgp@gmail.com>
2021-11-05 18:27:58 +0300

    fix error at app routing for loadChildren

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
     ]
   },
   {
-    path: 'admin', loadChildren: './admin/admin.module.ts'
+    path: 'admin', loadChildren: ()=> import('./admin/admin.module').then(m => m.AdminModule)
   }
 ];
 
