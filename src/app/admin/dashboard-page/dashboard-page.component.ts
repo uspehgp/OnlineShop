b17b6a2b556f45647619acc93ed7a572cfcc25f2
Uspehgp <uspehgp@gmail.com>
2021-11-10 16:44:07 +0300

    create dashboard page for view and remove products

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ProductService} from "../../shared/product.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  products = [];
+  rSub: Subscription;
+  pSub: Subscription;
 
-  ngOnInit(): void {
+  constructor(private productServ: ProductService) {
   }
 
+  ngOnInit(): void {
+    this.pSub = this.productServ.getAll().subscribe(products => {
+        console.log(products)
+        this.products = products
+      }
+    )
+  }
+
+  ngOnDestroy() {
+    if (this.pSub) {
+      this.pSub.unsubscribe();
+    }
+    if (this.rSub) {
+      this.rSub.unsubscribe();
+    }
+  }
+
+  remove(id) {
+    this.rSub = this.productServ.remove(id).subscribe(()=>{
+      this.products = this.products.filter(product => product.id != id)
+    })
+  }
 }
--- src/app/shared/product.service.ts
+++ src/app/shared/product.service.ts
@@ -1,19 +1,20 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
-import { map } from 'rxjs/operators';
-import { FbResponse, Product } from './interfaces';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from 'src/environments/environment';
+import {map} from 'rxjs/operators';
+import {FbResponse, Product} from './interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor(private  http : HttpClient) { }
+  constructor(private  http: HttpClient) {
+  }
 
   create(product) {
     return this.http.post(`${environment.fbDbUrl}/products.json`, product)
-      .pipe(map( (res : FbResponse) => {
+      .pipe(map((res: FbResponse) => {
         return {
           ...product,
           id: res.name,
@@ -23,10 +24,10 @@ export class ProductService {
   }
 
   getAll() {
-    return this.http.get(`${environment.fbDbUrl}/product.json`)
-      .pipe( map ( res => {
+    return this.http.get(`${environment.fbDbUrl}/products.json`)
+      .pipe(map(res => {
         return Object.keys(res)
-          .map( key => ({
+          .map(key => ({
             ...res[key],
             id: key,
             date: new Date(res[key].date)
@@ -35,14 +36,22 @@ export class ProductService {
   }
 
   getById(id) {
-    return this.http.get(`${environment.fbDbUrl}/product/${id}.json`)
-      .pipe( map ( (res: Product) => {
+    return this.http.get(`${environment.fbDbUrl}/products/${id}.json`)
+      .pipe(map((res: Product) => {
         return {
           ...res,
           id,
           date: new Date(res.date)
         }
       }))
+  }
+
+  remove(id) {
+    return this.http.delete(`${environment.fbDbUrl}/products/${id}.json`)
+  }
+
+  update(product: Product){
+    return this.http.patch(`${environment.fbDbUrl}/products/${product.id}.json`, product)
   }
 
 }
